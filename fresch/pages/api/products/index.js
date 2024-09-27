ff05b4fd35a6110a533c8ccd771642fff64a9fd3
Tobias Gretler <tobias.gretler@axonivy.com>
2024-09-27 12:48:41 +0200

    fiy cors

--- fresch/pages/api/products/index.js
+++ fresch/pages/api/products/index.js
@@ -1,8 +1,17 @@
 import { PrismaClient } from '@prisma/client';
+import Cors from 'nextjs-cors';
 
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
+  // Setze die CORS-Header
+  await Cors(req, res, {
+    methods: ['GET', 'POST', 'OPTIONS'], // Erlaube die benötigten HTTP-Methoden
+    origin: '*', // Setze die erlaubte Origin (kann auch spezifisch gesetzt werden)
+    optionsSuccessStatus: 200, // Für ältere Browser, die 204 nicht unterstützen
+  });
+
+  // Handle GET request
   if (req.method === 'GET') {
     try {
       const products = await prisma.product.findMany();
@@ -10,7 +19,9 @@ export default async function handler(req, res) {
     } catch (error) {
       res.status(500).json({ error: 'Error fetching products' });
     }
-  } else if (req.method === 'POST') {
+  }
+  // Handle POST request
+  else if (req.method === 'POST') {
     try {
       const { code, name, description, image, price, category, quantity, inventoryStatus, rating } = req.body;
       const newProduct = await prisma.product.create({
@@ -30,7 +41,9 @@ export default async function handler(req, res) {
     } catch (error) {
       res.status(500).json({ error: 'Error creating product' });
     }
-  } else {
+  }
+  // Handle other request methods
+  else {
     res.status(405).json({ message: 'Method not allowed' });
   }
 }
