cb6c74febfdebc4efd63443df7b178a1f6139fc7
ivy-tgr <tobias.gretler@axonivy.com>
2024-12-04 10:34:58 +0100

    feat: add AboutUs component and integrate it into the about page

--- fresch/pages/about.js
+++ fresch/pages/about.js
@@ -1,15 +1,56 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '@/components/Footer';
+import { Timeline } from 'primereact/timeline';
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+import { Image } from 'primereact/image';
+import AboutUs from '@/components/AboutUs';
+        
 
-const about = () => {
+export default function about() {
+        const events = [
+        { status: 'Ordered', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg' },
+        { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+        { status: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800' },
+        { status: 'Delivered', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B' }
+    ];
+
+    const customizedMarker = (item) => {
+        return (
+            <span className="flex w-2rem h-2rem align-items-center justify-content-center text-white border-circle z-1 shadow-1" style={{ backgroundColor: item.color }}>
+                <i className={item.icon}></i>
+            </span>
+        );
+    };
+
+    const customizedContent = (item) => {
+        return (
+            <Card title={item.status} subTitle={item.date}>
+                { item.image && <img src={`https://primefaces.org/cdn/primereact/images/product/${item.image}`} alt={item.name} width={200} className="shadow-1" />}
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
+                    quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
+                <Button label="Read more" className="p-button-text"></Button>
+            </Card>
+        );
+    };
+
     return (
-        <div>
+        <div className="card">
             <Navbar/>
-            <h1>about</h1>
+            <div className="card">
+                <Card title="Simple Card">
+                    <p className="m-0">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                        numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+                    </p>
+                    <Image src="https://primefaces.org/cdn/primereact/images/galleria/galleria7.jpg" alt="Image" width="250" />
+                </Card>
+            </div>
+            <Timeline value={events} align="alternate" className="customized-timeline" marker={customizedMarker} content={customizedContent} />
+            <AboutUs/>
             <Footer/>
         </div>
-    );
-};
-
-export default about;
+    )
+}
+        
