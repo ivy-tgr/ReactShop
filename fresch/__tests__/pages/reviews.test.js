9dbf37b012d850cc5f7472538340419257364352
ivy-tgr <tobias.gretler@axonivy.com>
2024-12-11 14:12:29 +0100

    test: remove obsolete reviews page test file

--- fresch/__tests__/pages/reviews.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import ReviewsPage from '../../pages/reviews';
-
-// test('renders reviews page', () => {
-//     render(<ReviewsPage />);
-//     const navbarElement = screen.getByRole('navigation');
-//     expect(navbarElement).toBeInTheDocument();
-// });
