29d9c5511af840a4b7deeaac3fe88f9eb0baa2d1
ivy-tgr <tobias.gretler@axonivy.com>
2025-01-10 12:40:20 +0100

    fix: update login page layout and change alert message to German

--- fresch/pages/auth/login.js
+++ fresch/pages/auth/login.js
@@ -11,34 +11,18 @@ export default function Login() {
     if (result.ok) {
       window.location.href = '/';
     } else {
-      alert('Login failed!');
+      alert('Login fehlgeschlagen!');
     }
   };
 
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <h1 className="login-title">Login</h1>
-        <form onSubmit={handleSubmit} className="login-form">
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="input-field"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="input-field"
-          />
-          <button type="submit" className="submit-button">Login</button>
-        </form>
-      </div>
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
